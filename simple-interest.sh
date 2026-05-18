c4d22818e75b8fc775e6a9b1968ff55dbe90c78a
tech-triad <tech@triad.earth>
2026-05-18 16:02:27 +0400

    Final grader fix v2

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,26 +1,6 @@
 #!/bin/bash
-
-# This script calculates simple interest given principal, annual rate of interest and time period in years.
-
-# Do not use this in production. Sample purpose only.
-
-# Author: Upkar Lidder (IBM)
-
-# Additional Authors:
-
-# tech-triad
-
-# Input:
-
-# p, principal amount
-
-# t, time period in years
-
-# r, annual rate of interest
-
-# Output:
-
-# simple interest = p*t*r
+# This script calculates simple interest given principal,
+# annual rate of interest and time period in years.
 
 echo "Enter the principal:"
 read p

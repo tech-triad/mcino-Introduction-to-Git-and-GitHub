367cb60403d1d50f611e2302cbb72d7294bedd1b
tech-triad <tech@triad.earth>
2026-05-18 15:50:38 +0400

    Final exact IBM format fix

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,5 +1,1 @@
-# Contributing
-
 All contributions, bug reports, bug fixes, documentation improvements, enhancements, and ideas are welcome.
-
-Please submit issues and pull requests for any improvements.
--- simple-interest.sh
+++ simple-interest.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
-# This script calculates simple interest given principal,
+# This script calculates simple interest given principal, annual rate of interest and time period in years.
 
-# annual rate of interest and time period in years.
+# Do not use this in production. Sample purpose only.
+
+# Author: Upkar Lidder (IBM)
+
+# Additional Authors:
+
+# tech-triad
+
+# Input:
+
+# p, principal amount
+
+# t, time period in years
+
+# r, annual rate of interest
+
+# Output:
+
+# simple interest = p*t*r
 
 echo "Enter the principal:"
 read p
